b9d3c29d88fbee99823aef6d01a763535cd5eea3
Krauchelli <mekaku26shi@gmail.com>
2025-06-07 14:42:27 +0700

    refactor: enhance Health class with animator integration and damage handling

--- Assets/Character/Scripts/Health/Health.cs
+++ Assets/Character/Scripts/Health/Health.cs
@@ -4,10 +4,13 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; } // tujuannya adalah untuk mengakses currentHealth dari luar kelas ini, tapi tidak bisa mengubahnya dari luar kelas ini
+    private Animator anim;
+    private bool isDead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage)
@@ -17,17 +20,25 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             // player hurt
+            anim.SetTrigger("hurt");
         }
         else
         {
-            // player dead
+            if (!isDead)
+            {
+                anim.SetTrigger("die");
+                // Disable the character controller or any other components that should not be active when dead
+                GetComponent<PlayerMovement>().enabled = false; // Assuming you have a CharacterController2D component
+                // player dead
+                isDead = true;
+            }
         }
     }
 
     //sample tes untuk mengurangi health
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             TakeDamage(1);
             Debug.Log("Current Health: " + currentHealth);
